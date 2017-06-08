94166d86401d1925148b986e1ec761e68f6769f5
kobake <kobake@users.sourceforge.net>
2017-06-08 13:45:18 +0900

    Add Controller and Views for area1 and area2

---
+++ AspNetCoreAreaSample/Areas/area1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreAreaSample.Area1.Controllers
+{
+    [Area("area1")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "AREA1 Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "AREA1 Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
